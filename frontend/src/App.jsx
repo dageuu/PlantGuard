86235ce82ff2c628e2f5927533559e2ff746f097
dageuu <bocadodaeyam@gmail.com>
2025-07-23 18:25:40 +0800

    Environment Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+// src/App.jsx
+import React from 'react';
+import LandingPg from './pages/LandingPg';
+
+function App() {
+  return <LandingPg />;
+}
+
+export default App;
---
+++ frontend/src/pages/LandingPg.jsx
@@ -0,0 +1,11 @@
+// src/pages/LandingPage.jsx
+export default function LandingPage() {
+    return (
+      <div className="min-h-screen bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 flex items-center justify-center">
+        <h1 className="text-white text-5xl font-bold drop-shadow-lg">
+          Welcome to PlantGuard 🌿
+        </h1>
+      </div>
+    );
+  }
+  
