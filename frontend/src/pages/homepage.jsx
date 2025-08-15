f648c2910ca033a3f73f394a82f318bae5074fb4
dageuu <bocadodaeyam@gmail.com>
2025-08-15 20:56:46 +0800

    Updated Tailwind version and fixed some environmental errors

---
+++ frontend/src/pages/homepage.jsx
@@ -0,0 +1,10 @@
+export default function Homepage() {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-green-50">
+        <h1 className="text-4xl font-bold text-green-700">
+          Welcome to PlantGuard 🌱
+        </h1>
+      </div>
+    );
+  }
+  
