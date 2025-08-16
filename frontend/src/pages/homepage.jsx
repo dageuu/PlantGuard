931e9ffb9dae666fbb161a2f27c9dc0ea12694f1
dageuu <bocadodaeyam@gmail.com>
2025-08-16 21:34:10 +0800

    minor changes lang di pa makaisip ng magandang UI, continue bukas.

--- frontend/src/pages/homepage.jsx
+++ frontend/src/pages/homepage.jsx
@@ -1,10 +1,21 @@
 export default function Homepage() {
     return (
-      <div className="min-h-screen flex items-center justify-center bg-green-50">
-        <h1 className="text-4xl font-bold text-green-700">
+      <>
+      <header className="bg-amber-950 text-white p-4 shadow-md">
+        <h1 className="text-2xl ">PlantGuard</h1>
+      </header>
+      {/*<div className="min-h-screen min-w-screen flex items-center justify-center bg gradient-to-tl from-green-200 from-20% via-green-300 to-green-865">*/}
+      <div className="relative min-h-screen min-w-screen flex items-center justify-center bg-[url(src/assets/brightleavesbg.jpg)] bg-cover">
+      <div className="absolute inset-0 bg-white opacity-35"></div>
+      <section className="section_one bg-amber-200 rounded-md">
+        <h1 className="relative text-4xl text-amber-950">
           Welcome to PlantGuard 🌱
         </h1>
+      </section>
       </div>
+      <footer className=" bg-green-500 text-white p-4 text-center">
+        <p>&copy; 2023 PlantGuard. All rights reserved.</p>
+      </footer>
+      </>
     );
   }
-  
